4f6f3e7d66ce72f4d72188f3e146d2aad5095280
Albino1995 <763968024@qq.com>
2017-09-13 16:21:42 +0800

    首页导航ajax传输数据

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import Layout from './components/layout'
 import IndexPage from './pages/index'
-import VueRouter from 'vue-router'
+
 Vue.use(VueRouter)
+Vue.use(VueResource)
+
 let router = new VueRouter({
   mode: 'history',
   routes: [
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -44,103 +44,31 @@
 
 <script>
   export default {
+    created: function () {
+      this.$http.get('api/getNewsList')
+        .then((res) => {
+          this.newsList = res.data
+        }, (err) => {
+          console.log(err)
+        })
+      this.$http.get('api/getBoardList')
+        .then((res) => {
+          this.boardList = res.data
+        }, (err) => {
+          console.log(err)
+        })
+      this.$http.get('api/getProductList')
+        .then((res) => {
+          this.productList = res.data
+        }, (err) => {
+          console.log(err)
+        })
+    },
     data () {
       return {
-        boardList: [
-          {
-            title: '开放产品',
-            description: '开放产品是一款开放产品',
-            id: 'car',
-            toKey: 'analysis',
-            saleout: false
-          },
-          {
-            title: '品牌营销',
-            description: '品牌营销帮助你的产品更好地找到定位',
-            id: 'earth',
-            toKey: 'count',
-            saleout: false
-          },
-          {
-            title: '使命必达',
-            description: '使命必达快速迭代永远保持最前端的速度',
-            id: 'loud',
-            toKey: 'forecast',
-            saleout: true
-          },
-          {
-            title: '勇攀高峰',
-            description: '帮你勇闯高峰，到达事业的顶峰',
-            id: 'hill',
-            toKey: 'publish',
-            saleout: false
-          }
-        ],
-        newsList: [
-          {
-            title: '数据统计',
-            url: 'http://starcraft.com'
-          },
-          {
-            title: '数据预测',
-            url: 'http://warcraft.com'
-          },
-          {
-            title: '流量分析',
-            url: 'http://overwatch.com'
-          },
-          {
-            title: '广告发布',
-            url: 'http://hearstone.com'
-          }
-        ],
-        productList: {
-          pc: {
-            title: 'PC产品',
-            list: [
-              {
-                name: '数据统计',
-                url: 'http://starcraft.com'
-              },
-              {
-                name: '数据预测',
-                url: 'http://warcraft.com'
-              },
-              {
-                name: '流量分析',
-                url: 'http://overwatch.com',
-                hot: true
-              },
-              {
-                name: '广告发布',
-                url: 'http://hearstone.com'
-              }
-            ]
-          },
-          app: {
-            title: '手机应用类',
-            last: true,
-            list: [
-              {
-                name: '91助手',
-                url: 'http://weixin.com'
-              },
-              {
-                name: '产品助手',
-                url: 'http://twitter.com',
-                hot: true
-              },
-              {
-                name: '智能地图',
-                url: 'http://maps.com'
-              },
-              {
-                name: '团队语音',
-                url: 'http://phone.com'
-              }
-            ]
-          }
-        }
+        boardList: [],
+        newsList: [],
+        productList: {}
       }
     }
   }
@@ -257,8 +185,11 @@
   .new-item {
     display: inline-block;
     width: 230px;
+    /*超过边界隐藏*/
     overflow: hidden;
+    /*文本溢出...处理*/
     text-overflow: ellipsis;
+    /*禁止换行*/
     white-space: nowrap;
   }
 </style>
