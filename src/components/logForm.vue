dee077ce180f0e8ca136fdbaf758e06ae018f8ac
Albino1995 <763968024@qq.com>
2017-09-14 17:32:32 +0800

    更新登录

---
+++ src/components/logForm.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="login-form">
+    <div class="g-form">
+      <div class="g-form-line">
+        <span class="g-form-label">用户名：</span>
+        <div class="g-form-input">
+          <input type="text"
+          v-model="usernameModel" placeholder="请输入用户名" autocomplete="off">
+        </div>
+        <span class="g-form-error">{{ userErrors.errorText }}</span>
+      </div>
+      <div class="g-form-line">
+        <span class="g-form-label">密码：</span>
+        <div class="g-form-input">
+          <input type="password"
+          v-model="passwordModel" placeholder="请输入密码">
+        </div>
+        <span class="g-form-error">{{ passwordErrors.errorText }}</span>
+      </div>
+      <div class="g-form-line">
+        <div class="g-form-btn">
+          <a class="button" @click="onLogin">登录</a>
+        </div>
+      </div>
+      <p>{{ errorText }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      usernameModel: '',
+      passwordModel: '',
+      errorText: ''
+    }
+  },
+  computed: {
+    userErrors () {
+      let errorText, status
+      if (!/.+@.+/g.test(this.usernameModel)) {
+        status = false
+        errorText = '不是合法的邮箱地址'
+      } else {
+        status = true
+        errorText = ''
+      }
+      if (!this.userFlag) {
+        errorText = ''
+        this.userFlag = true
+      }
+      return {
+        status,
+        errorText
+      }
+    },
+    passwordErrors () {
+      let errorText, status
+      if (!/^\w{6,12}$/g.test(this.passwordModel)) {
+        status = false
+        errorText = '请输入6-12位密码'
+      } else {
+        status = true
+        errorText = ''
+      }
+      if (!this.passwordFlag) {
+        errorText = ''
+        this.passwordFlag = true
+      }
+      return {
+        status,
+        errorText
+      }
+    }
+  },
+  methods: {
+    onLogin () {
+      if (!this.userErrors.status || !this.passwordErrors.status) {
+        this.errorText = '部分选项未通过'
+      } else {
+        this.errorText = ''
+        this.$http.post('api/login')
+          .then((res) => {
+            this.$emit('has-log', res.data)
+          }, (error) => {
+            console.log(error)
+          })
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .g-form p{
+    color: red;
+  }
+</style>
