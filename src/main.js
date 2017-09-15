704dbb3bbf7c15c46dbb68580eb61c5a6fd4334d
Albino1995 <763968024@qq.com>
2017-09-15 16:46:17 +0800

    更新详情页模板

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 import Layout from './components/layout'
 import IndexPage from './pages/index'
+import DetailPage from './pages/detail'
+import Analysis from './pages/detail/analysis'
+import Count from './pages/detail/count'
+import Forecast from './pages/detail/forecast'
+import Publish from './pages/detail/Publish'
 
 Vue.use(VueRouter)
 Vue.use(VueResource)
@@ -13,6 +18,29 @@ let router = new VueRouter({
     {
       path: '/',
       component: IndexPage
+    },
+    {
+      path: '/detail',
+      component: DetailPage,
+      redirect: '/',
+      children: [
+        {
+          path: 'analysis',
+          component: Analysis
+        },
+        {
+          path: 'count',
+          component: Count
+        },
+        {
+          path: 'forecast',
+          component: Forecast
+        },
+        {
+          path: 'publish',
+          component: Publish
+        }
+      ]
     }
   ]
 })
